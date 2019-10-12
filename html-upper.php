2c2cb1aed1d8a4d57b869210ce590cf22a782f33
yonekatsuya <51873173+yonekatsuya@users.noreply.github.com>
2019-10-12 22:35:57 +0900

    Create html-upper.php

---
+++ html-upper.php
@@ -0,0 +1,16 @@
+<?php
+function html_upper() {
+print '<!doctype html>';
+print '<html lang="ja">';
+print '<head>';
+print '<meta charset="utf-8">';
+print '<title>ProMatch</title>';
+print '<link rel="
+stylesheet" href="style.css">';
+print '</head>';
+print '<body style="background-color:#99cc33;height:100%;">';
+print '<div style="height:100%;min-height:100vh;
+position:relative;padding-bottom:100px;box-sizing:border-box;
+">';
+}
+?>
